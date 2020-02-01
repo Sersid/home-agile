05a86eabed765f1ad91a1b90b806f1af1b34316b
Sersid <sersid_bro@mail.ru>
2020-02-01 20:03:52 +0300

    Updated TermChanger widget

--- resources/js/components/ticket/TermChanger.vue
+++ resources/js/components/ticket/TermChanger.vue
@@ -1,19 +1,15 @@
 <template>
-    <div class="d-flex">
-        <button class="btn btn-icon fs-lg mr-2 active" type="button" @click="show = !show">
-            <i class="fal fa-calendar-alt" ref="button"></i>
-        </button>
-        <datepicker v-if="show" :language="ru" input-class="form-control" v-model="date" format="dd MMM yyyy" />
-        <span v-else class="pt-2">
-            <span v-if="hasTerm">{{dateFormatted}}</span>
-            <span v-else class="text-muted cursor-pointer" @click="$refs.button.click()">не указан</span>
-        </span>
+    <div>
+        <input :value="dateValue" @input="save($event.target.value)" class="form-control" type="date" v-if="show"/>
+        <div @click="showInput" class="pt-2 cursor-pointer" v-else>
+            <i class="fal fa-calendar-alt mr-1"></i>
+            <span v-if="hasDate">{{dateFormatted}}</span>
+            <span class="text-muted" v-else>не указан</span>
+        </div>
     </div>
 </template>
 
 <script>
-    import Datepicker from 'vuejs-datepicker';
-    import {ru} from 'vuejs-datepicker/dist/locale'
     import moment from 'moment';
 
     export default {
@@ -24,32 +20,35 @@
                 required: true
             }
         },
-        components: {Datepicker},
         data() {
             return {
-                data: Object,
-                ru: ru,
                 show: false,
                 date: ''
             };
         },
         created() {
-            this.data = this.ticket;
+            this.date = this.ticket.term;
         },
         computed: {
-            hasTerm() {
-                return this.data.term !== null;
+            hasDate() {
+                return this.date !== null && this.date.length > 0;
             },
             dateFormatted() {
-                return moment(this.data.term).format('D MMMM YYYY');
+                return moment(this.date).format('D MMMM YYYY');
             },
+            dateValue() {
+                return this.hasDate ? moment(this.date).format('YYYY-MM-D') : ''
+            }
         },
-        watch: {
-            date(date) {
-                this.$set(this.data, 'term', date);
+        methods: {
+            showInput() {
+                this.show = true;
+            },
+            save(value) {
                 this.show = false;
+                this.date = value;
                 this.$emit('process', true);
-                axios.patch('ticket/term/' + this.data.id, {term: date})
+                axios.patch('ticket/term/' + this.ticket.id, {term: this.date})
                     .then(response => {
                         this.$emit('saved', response.data);
                     })
